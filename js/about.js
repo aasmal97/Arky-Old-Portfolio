1ce81e17c66644ff5c80521d1eb38df117bd74a5
Arky Asmal <arkyasmal@gmail.com>
2021-07-24 01:40:03 -0400

    "timeline scroll optimized"

--- js/about.js
+++ js/about.js
@@ -1,5 +1,7 @@
 const timeline = document.getElementById("timeline")
 const timelineBlock= timeline.querySelectorAll("div")
+
+//inserts lines connecting timeline automatically 
 document.addEventListener("DOMContentLoaded", function() {
     for (key in Object.keys(timelineBlock)){
         let lines = document.createElement("div")
@@ -14,7 +16,8 @@ document.addEventListener("DOMContentLoaded", function() {
         timeline.insertBefore(lines, timelineBlock[key].nextElementSibling)
     }
 });
-let timelineCounter = 0
+
+let scrollDisabled = false;
 const observer = new IntersectionObserver(function(entries) {
     let el = entries[0].target.getBoundingClientRect()
 	// isIntersecting is true when element and viewport are overlapping
@@ -27,16 +30,48 @@ const observer = new IntersectionObserver(function(entries) {
             // If not, the accuracy of offset diminshes
             //setTimeout is set to 100 for the same reason. 
             let offset = window.pageYOffset
-            console.log("stop")
-            
             setTimeout(function(){
                 window.scrollTo({
                     top: el.top + offset - navbar.offsetHeight,
                     behavior: 'smooth',
                 }) 
-                console.log("hello")
             },100)
+
+            //signal timeline animations can start playing
+            scrollDisabled = true
         } 
     }, { threshold: [1] });
 
+let timelineCounter = 0
+let timeItems = document.getElementById("timeline").querySelectorAll(".item")
+let timeItemsLength = Object.keys(timeItems).length
+
+function timelineAnimate(){
+    
+    if (scrollDisabled === true){
+        //tracks how many animations
+        timelineCounter = timelineCounter + 1
+
+        //continue scrolling, and stop observing, if animations are done
+        if(timelineCounter>=timeItemsLength){
+            document.querySelector("body").classList.remove("stopScroll")
+            observer.unobserve(document.querySelector("#about"));
+        }
+
+        //disable firing too fast, for performance and animation complete
+        scrollDisabled = false
+        setTimeout(function(){
+            scrollDisabled = true
+        },400)
+    }
+}
+
 observer.observe(document.querySelector("#about"));
+
+document.addEventListener("wheel", function(e){
+    timelineAnimate()
+})
+
+document.addEventListener("touchend", function(e){
+    timelineAnimate()
+})
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,19 +1,24 @@
 //for links in navbar
 //scroll action
-let regex = /#\w*/
+let hrefRegex = /#\w*/
 function smoothScroll(e=0){
     e.preventDefault();
-    const targetElement = document.querySelector(e.target.href.match(regex)[0]).getBoundingClientRect()
+    const targetElement = document.querySelector(e.target.href.match(hrefRegex)[0]).getBoundingClientRect()
+    let offSet = window.pageYOffset
     window.scrollTo({
-        top: targetElement.top + window.pageYOffset - navbar.offsetHeight,
+        top: targetElement.top + offSet - navbar.offsetHeight,
         behavior: 'smooth',
     })
 }
 
 for(key in Object.keys(navLinks)){
     navLinks[key].addEventListener("click", function(e){
+        if(this.href.match(hrefRegex)[0] !== "#about"){
+            document.querySelector("body").classList.remove("stopScroll")
+            scrollDisabled = false
+        }
         //For skill btn only, it unhides the list first since its under a .4s transition
-        if(e.target.href.match(regex)[0]==="#skillsBtn" && skills.classList.contains("hidden")){
+        if(e.target.href.match(hrefRegex)[0]==="#skillsBtn" && skills.classList.contains("hidden")){
             showSkills()
             setTimeout(function(){
                 smoothScroll(e);
