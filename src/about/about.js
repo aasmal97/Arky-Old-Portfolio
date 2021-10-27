6d81bc9a8d3ead5e3753050d5c82aeb3a6463731
Arky <arkyasmal@gmail.com>
2021-10-26 21:21:57 -0400

    "adjusting overflow ovrflow for computers. Mobile devices by default have this applied"

--- src/about/about.js
+++ src/about/about.js
@@ -99,7 +99,7 @@ const timelineAnimate = () =>{
             scrollDisabled = false
             setTimeout(function(){
                 disableScoll()
-            },500)
+            },250)
         }
     }
 }
